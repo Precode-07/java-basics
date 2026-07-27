deb2f65bad44a2f23529833767f8724f3655ddc4
Precode-07 <prem6150100@gmail.com>
2026-07-27 23:55:10 +0530

    Resolve merge conflict

---
+++ literals.java
@@ -0,0 +1,22 @@
+class literals {
+    public static void main(String[] args) {
+        char c = 'A';
+        byte b = 12;
+        short s = 1232;
+        int i = 1232112;
+        long l = 12138767288l;
+        float f = 2.3f;
+        double d = 32.22112;
+        boolean B = true;
+
+        System.out.println("Character : "+c);
+        System.out.println("Byte : "+b);
+        System.out.println("Short : "+s);
+        System.out.println("Integer : "+i);
+        System.out.println("Long : "+l);
+        System.out.println("Float : "+f);
+        System.out.println("Double : "+d);
+        System.out.println("Boolean : "+B);
+
+    }
+}
