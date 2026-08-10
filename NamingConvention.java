966bc03c992333e6996b0f67453e493986551b3f
Precode-07 <prem6150100@gmail.com>
2026-08-10 23:44:04 +0530

    Naming Convention in Java

---
+++ NamingConvention.java
@@ -0,0 +1,27 @@
+// Classes and Interfaces - PascalCase
+class Calculate {
+
+    // Constants - UPPERCASE with underscores
+    static final double PI = 3.14;
+
+    // Methods - camelCase
+    public int addition(int x, int y) {
+        return x + y;
+    }
+
+    public double radius(int r) {
+        return r * PI;
+    }
+}
+
+public class NamingConvention {
+    public static void main(String[] args) {
+        Calculate calc = new Calculate();
+        // Variables - uses camelCase
+        int a = 9;
+        int b = 7;
+
+        System.out.println(calc.addition(a, b)); // 16
+        System.out.println(calc.radius(a));      // 28.26
+    }
+}
